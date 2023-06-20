0dafd6418610696ecbc1d7f62dfdc48165b40d5a
Aleksandr27041986 <sanila27041986@yandex.ru>
2023-06-20 10:28:14 +0300

    Fixed allowed hosts in settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,12 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '158.160.64.163',
+    '127.0.0.1',
+    'localhost',
+    'taskisanila27041986.servebeer.com',
+]
 
 
 # Application definition
